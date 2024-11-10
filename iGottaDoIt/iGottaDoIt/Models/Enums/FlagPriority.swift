811a0be9bbe394227683a957f8fcdadee013e1b9
RJ <radek.jenik36@gmail.com>
2024-11-10 13:10:50 +0100

    feat: flagPriority color

--- iGottaDoIt/iGottaDoIt/Models/Enums/FlagPriority.swift
+++ iGottaDoIt/iGottaDoIt/Models/Enums/FlagPriority.swift
@@ -5,6 +5,9 @@
 //  Created by Radek Jeník on 11/10/24.
 //
 
+import Foundation
+import SwiftUI
+
 enum FlagPriority: Int {
     case veryLow = 1
     case low = 2
@@ -14,16 +17,31 @@ enum FlagPriority: Int {
 
     var description: String {
         switch self {
+            case .veryLow:
+                return "Very Low Priority"
+            case .low:
+                return "Low Priority"
+            case .medium:
+                return "Medium Priority"
+            case .high:
+                return "High Priority"
+            case .veryHigh:
+                return "Very High Priority"
+        }
+    }
+    
+    var getPriorityInColor: Color{
+        switch self{
         case .veryLow:
-            return "Very Low Priority"
+            return .white
         case .low:
-            return "Low Priority"
+            return .yellow
         case .medium:
-            return "Medium Priority"
+            return .orange
         case .high:
-            return "High Priority"
+            return .purple
         case .veryHigh:
-            return "Very High Priority"
+            return .red
         }
     }
 }
