4d7e432d32f165a0710ba1e148c2c0be6d487838
RJ <radek.jenik36@gmail.com>
2024-11-13 16:57:16 +0100

    feat: detail design updated

--- iGottaDoIt/iGottaDoIt/Views/TaskDetailView.swift
+++ iGottaDoIt/iGottaDoIt/Views/TaskDetailView.swift
@@ -12,22 +12,48 @@ struct TaskDetailView: View {
     @Bindable var task: Task
     
     var body: some View {
-        Text("Item at \(task.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-        
-        Text("Task subject: \(task.note)")
-        
-        Text("Task flag: \(task.flag.description)")
-            .background{
-                task.flag.priority.getPriorityInColor
+        VStack(alignment: .leading, content: {
+            TextField("", text: $task.note, axis: .vertical)
+                .font(.title2)
+                .bold()
+                .padding()
+            
+            VStack{
+                HStack{
+                    task.flag.image
+                        
+                    Text("\(task.flag.priority.description)")
+                }
+                .onTapGesture {
+                    task.flag.setHigherPriority()
+                }
+                .padding()
             }
+            
+            Text("Task is being done since \(task.created, format: Date.FormatStyle(date: .complete, time: .shortened)) and is to be done until \(task.until, format: Date.FormatStyle(date: .complete, time: .shortened)).")
+                .padding()
+            
+            if task.finishedDate != nil{
+                Text("Task has been done on \(task.finishedDate!, format: Date.FormatStyle(date: .complete, time: .shortened))")
+                    .padding(.horizontal)
+            }
+            
+            
+            Spacer()
+        })
+        .padding()
         
-        Button(action: updateTime){
-            Label("Update time", systemImage: "arrow.clockwise")
+        VStack(alignment: .center){
+            Button(action: finishTask){
+                Label(self.task.done ? "Task is done" : "Task is not done", systemImage: self.task.done ? "checkmark.circle.fill" : "circle")
+            }
+            .padding()
         }
+        
     }
     
-    private func updateTime(){
-        self.task.timestamp = .now
+    private func finishTask(){
+        self.task.completeTask()
     }
 }
 
